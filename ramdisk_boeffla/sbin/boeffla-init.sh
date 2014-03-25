7ea34b6c36a44a8d4c85a76a842268d83fcf2d3b
andip71 <andreasp@gmx.de>
2014-03-25 16:08:32 +0100

    Boeffla ramfs: Fix autoroot not working in some cases
    now all files will be replaced hard, regardless what exists or not

--- ramdisk_boeffla/sbin/boeffla-init.sh
+++ ramdisk_boeffla/sbin/boeffla-init.sh
@@ -235,35 +235,32 @@ FRANDOM_ENABLER="/data/.boeffla/enable-frandom"
 
 		echo $(date) Auto root is enabled >> $BOEFFLA_LOGFILE
 
-		if [ ! -f /system/xbin/su ] && [ ! -f /system/bin/su ]; then
+		mount -o remount,rw -t ext4 $SYSTEM_DEVICE /system
 
-			mount -o remount,rw -t ext4 $SYSTEM_DEVICE /system
+		/sbin/busybox mkdir /system/bin/.ext
+		/sbin/busybox cp /res/misc/su /system/xbin/su
+		/sbin/busybox cp /res/misc/su /system/xbin/daemonsu
+		/sbin/busybox cp /res/misc/su /system/bin/.ext/.su
+		/sbin/busybox cp /res/misc/install-recovery.sh /system/etc/install-recovery.sh
+		/sbin/busybox echo /system/etc/.installed_su_daemon
+		
+		/sbin/busybox chown 0.0 /system/bin/.ext
+		/sbin/busybox chmod 0777 /system/bin/.ext
+		/sbin/busybox chown 0.0 /system/xbin/su
+		/sbin/busybox chmod 6755 /system/xbin/su
+		/sbin/busybox chown 0.0 /system/xbin/daemonsu
+		/sbin/busybox chmod 6755 /system/xbin/daemonsu
+		/sbin/busybox chown 0.0 /system/bin/.ext/.su
+		/sbin/busybox chmod 6755 /system/bin/.ext/.su
+		/sbin/busybox chown 0.0 /system/etc/install-recovery.sh
+		/sbin/busybox chmod 0755 /system/etc/install-recovery.sh
+		/sbin/busybox chown 0.0 /system/etc/.installed_su_daemon
+		/sbin/busybox chmod 0644 /system/etc/.installed_su_daemon
 
-			/sbin/busybox mkdir /system/bin/.ext
-			/sbin/busybox cp /res/misc/su /system/xbin/su
-			/sbin/busybox cp /res/misc/su /system/xbin/daemonsu
-			/sbin/busybox cp /res/misc/su /system/bin/.ext/.su
-			/sbin/busybox cp /res/misc/install-recovery.sh /system/etc/install-recovery.sh
-			/sbin/busybox echo /system/etc/.installed_su_daemon
-			
-			/sbin/busybox chown 0.0 /system/bin/.ext
-			/sbin/busybox chmod 0777 /system/bin/.ext
-			/sbin/busybox chown 0.0 /system/xbin/su
-			/sbin/busybox chmod 6755 /system/xbin/su
-			/sbin/busybox chown 0.0 /system/xbin/daemonsu
-			/sbin/busybox chmod 6755 /system/xbin/daemonsu
-			/sbin/busybox chown 0.0 /system/bin/.ext/.su
-			/sbin/busybox chmod 6755 /system/bin/.ext/.su
-			/sbin/busybox chown 0.0 /system/etc/install-recovery.sh
-			/sbin/busybox chmod 0755 /system/etc/install-recovery.sh
-			/sbin/busybox chown 0.0 /system/etc/.installed_su_daemon
-			/sbin/busybox chmod 0644 /system/etc/.installed_su_daemon
+		/system/bin/sh /system/etc/install-recovery.sh
 
-			/system/bin/sh /system/etc/install-recovery.sh
-
-			mount -o remount,ro -t ext4 $SYSTEM_DEVICE /system
-			echo $(date) Auto root: su installed >> $BOEFFLA_LOGFILE
-		fi
+		mount -o remount,ro -t ext4 $SYSTEM_DEVICE /system
+		echo $(date) Auto root: su installed >> $BOEFFLA_LOGFILE
 
 		rm $SD_PATH/autoroot
 	fi

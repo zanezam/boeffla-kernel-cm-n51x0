d29a5fdf40c790aceae8747a11961a4ebc210ad6
ZaneZam <cyxman@yahoo.com>
2014-08-23 17:48:15 +0200

    samsung_battery: increased possible AC max charging rate to 1800mA in Charge Level Interface

--- include/linux/charge_level.h
+++ include/linux/charge_level.h
@@ -21,7 +21,7 @@ extern int ignore_safety_margin;
 
 #define AC_CHARGE_LEVEL_DEFAULT 1000
 #define AC_CHARGE_LEVEL_MIN 100
-#define AC_CHARGE_LEVEL_MAX 1600
+#define AC_CHARGE_LEVEL_MAX 1800
 
 #define USB_CHARGE_LEVEL_DEFAULT 475
 #define USB_CHARGE_LEVEL_MIN 0

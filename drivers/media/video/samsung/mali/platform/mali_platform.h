7b0aedb4262adc682bf836a5c930bd4d9069786a
ZaneZam <cyxman@yahoo.com>
2015-04-22 20:43:14 +0200

    mali: platform rewrite
    Samsung integrated checks on in the Pegasus platform that prevented the GPU control interfaces to work;
    on every decideNextStatus call the DVFS tables would be rewritten with separate fixed values.
    Module parameters for these values were present in the kernel so the hypothesis is that they were used
    for debugging during development.
    
    -All checks are removed and the driver now looks very similar to the Orion platform.
    -Removal of the CPU frequency lock to 1200MHz if the GPU is at 440MHz, this is excessive as 3D load
     heavy applications usually do not tax the CPU that far, and is an unnecessary power consumption burden.
    -The thermal control unit temperature throttling causes to fix the voltage to a fixed value when throttling
     is in place; this is useless considering frequency is not limited, making the whole thing senseless. Thus removed.
    -Cleaned up the files from unnecessary ifdef's for readability considering the fixed platform values.
    
    thx to AndreiLux
    
    ported to n80x0 by andip71 ported to work on CM sources for n5110 by ZaneZam

--- drivers/media/video/samsung/mali/platform/mali_platform.h
+++ drivers/media/video/samsung/mali/platform/mali_platform.h
@@ -27,8 +27,6 @@
 /* @Enable or Disable Mali GPU Bottom Lock feature */
 #define MALI_GPU_BOTTOM_LOCK 1
 
-#define MALI_VOLTAGE_LOCK 1
-
 /* @Enable or Disable the CPU frequency lock when the GPU clock is 440 Mhz */
 #define CPUFREQ_LOCK_DURING_440 0
 
@@ -136,15 +134,6 @@ int mali_dvfs_bottom_lock_pop(void);
 int mali_dvfs_bottom_lock_push(int lock_step);
 int mali_dvfs_bottom_lock_pop(void);
 #endif
-#endif
-
-int mali_dvfs_get_vol(int step);
-
-#if MALI_VOLTAGE_LOCK
-int mali_voltage_lock_push(int lock_vol);
-int mali_voltage_lock_pop(void);
-int mali_voltage_lock_init(void);
-int mali_vol_get_from_table(int vol);
 #endif
 
 #ifdef __cplusplus

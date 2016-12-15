d7ec5361f89145023be3740aac64f4255705c53c
ZaneZam <cyxman@yahoo.com>
2016-12-15 13:25:40 +0100

    Boeffla ramfs: Add filename template for kernel download

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -18,7 +18,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="n5100;cm;cm13.0;http://kernel.boeffla.de/zanezam/n5100/boeffla-kernel/"
+KERNEL_SPECS="n5100;cm;cm13.0;http://kernel.boeffla.de/zanezam/n5100/boeffla-kernel/;boeffla-kernel-#VERSION#-CM13.0-n5100-anykernel.recovery.zip"
 
 # kernel features
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
